845cf86898d0305371da9268bd9807b60608217c
hasnainraza1212 <105921794+hasnainraza1212@users.noreply.github.com>
2023-01-26 14:46:05 +0500

    javascript alert assignment completed

---
+++ App.js
@@ -0,0 +1,36 @@
+// question1
+// alert("Hello dear visitor, how are you??")
+
+
+// question1
+// alert("Error! Please enter  valid password")
+
+
+
+
+// question1
+// alert("Welcome to JS Land... <br> Happy coding!")
+
+
+
+
+// question1
+// alert("Welcome to JS Land...")
+// alert("Happy coding!")
+
+
+
+// question1
+// alert("Hello... i can use JS through my web browser's console")
+
+
+
+
+
+// question1
+
+
+
+
+
+// question1
